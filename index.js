f65329df38521372b65f1af61df8fa7929798e90
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-04-23 17:56:28 +0200

    comparer test

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const Hand = require('./Hand');
 
 // ♥♦♣♠
 
-let hand1 = new Hand('♥Q', '♥5', '♥2', '♥9', '♦K');
-let hand2 = new Hand('♠T', '♥8', '♠8', '♠K', '♦K');
+let hand1 = new Hand('♣2', '♥2', '♣2', '♥3', '♦3');
+let hand2 = new Hand('♣3', '♠3', '♦5', '♠8', '♦4');
 
 console.log(CompareHands.comparer(hand1, hand2));
 
