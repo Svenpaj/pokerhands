81ba0463b89c6bf369b0f9241d7a3e2dad19fac4
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-04-23 17:52:47 +0200

    comparer test ToBe syntax

--- tests/comparer.test.js
+++ tests/comparer.test.js
@@ -4,6 +4,6 @@ const suits = '♥♦♣♠';
 
 test('check that comparer returns hand1 as the winning hand', () => {
   let hand1 = new Hand('♣2', '♥2', '♣2', '♥3', '♦3');
-  let hand2 = new Hand('♦3', '♦3', '♦5', '♠8', '♦4');
+  let hand2 = new Hand('♣3', '♠3', '♦5', '♠8', '♦4');
   expect(CompareHands.comparer(hand1, hand2)).toBe(hand1)
 });
